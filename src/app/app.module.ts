56d7f10d9dd845b88eb907102e1a370b503fed6b
reza <rezamt@gmail.com>
2020-09-06 20:36:51 +1000

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { BackendService } from './backend.service';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactDetailComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    InMemoryWebApiModule.forRoot(BackendService),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/backend.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+
+import {InMemoryDbService} from 'angular-in-memory-web-api'
+
+@Injectable({
+  providedIn: 'root'
+})
+
+// The service class needs to implement InMemoryDbService and then override the createDb() method
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendService implements InMemoryDbService {
+
+  constructor() {
+  }
+
+  createDb() {
+    let contacts = [
+      {id: 1, name: 'Contact 1', email: 'contact1@email.com'},
+      {id: 2, name: 'Contact 2', email: 'contact2@email.com'},
+      {id: 3, name: 'Contact 3', email: 'contact3@email.com'},
+      {id: 4, name: 'Contact 4', email: 'contact4@email.com'}
+    ];
+
+    return {contacts};
+
+  }
+}
